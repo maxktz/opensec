bac4aeae00e0b5d7e70c617ced0b0b329a7a339f
Dominik K. <dominik@rivo.gg>
2026-05-15 09:41:28 +0200

    Declare twitter-image config inline (Next can't parse re-exports)

--- apps/web/src/app/donors/twitter-image.tsx
+++ apps/web/src/app/donors/twitter-image.tsx
@@ -1,1 +1,7 @@
-export { default, alt, size, contentType, runtime, revalidate } from "./opengraph-image";
+export { default } from "./opengraph-image";
+
+export const runtime = "nodejs";
+export const alt = "OpenSec Donors Leaderboard";
+export const size = { width: 1200, height: 630 };
+export const contentType = "image/png";
+export const revalidate = 300;
